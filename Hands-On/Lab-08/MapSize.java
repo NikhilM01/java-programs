9f5c2b548060b15c060bd456de61192771df3fbb
Nikhil Muneshwar <93129551+NikhilM01@users.noreply.github.com>
2024-06-25 13:07:37 +0530

    Update MapSize.java

--- Hands-On/Lab-08/MapSize.java
+++ Hands-On/Lab-08/MapSize.java
@@ -6,10 +6,10 @@ public class MapSize {
     public static void main(String[] args) {
         // Create a HashMap and add key-value pairs to it
         Map<Integer, String> map = new HashMap<>();
-        map.put(1, "Aniket");
-        map.put(2, "Bodhe");
-        map.put(3, "Andy");
-        map.put(4, "B");
+        map.put(1, "Nikhil");
+        map.put(2, "Anand");
+        map.put(3, "Sakshi");
+        map.put(4, "Aakansha");
 
         // Get the number of key-value mappings in the map
         int size = map.size();
